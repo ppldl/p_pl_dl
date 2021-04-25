72ad450d853340be4f69904d21eadd63ce6ec012
ppldl <ppldl@protonmail.com>
2021-04-24 22:27:01 -0500

    Video extraction from playlist will check whether the video URL has already been parsed to avoid duplicates Expanded Page_Xvideos to handle more of the playlist processing that was previously done inside the "run" method

--- p_pl_dl_xv.py
+++ p_pl_dl_xv.py
@@ -14,13 +14,6 @@ def run(sUrl, sCookieSource=None, nVideoLimit=None, bDebug=False):
     if dl_common.dCookiesParsed is None:
         print("WARNING :: No cookies were provided! Private videos/playlists will fail to download!\r\n")
 
-    if f'{sExtractor}.com/video' in sUrl:
-        sUrlType = 'video'
-    elif f'{sExtractor}.com/favorite' in sUrl:
-        sUrlType = 'playlist'
-    else:
-        raise ValueError(f"Unable to determine {sExtractor} URL type for {sUrl}! Please submit a bug report!")
-
     # Attempt initial connection
     html = dl_common.session.get(sUrl, headers=dl_common.dHeaders, cookies=dl_common.dCookiesParsed)
     print(f"Initial connection status: {html.status_code}")
@@ -36,52 +29,19 @@ def run(sUrl, sCookieSource=None, nVideoLimit=None, bDebug=False):
         text_file.write(html.text)
         text_file.close()
 
-    lUrlVideos = []
-    if sUrlType == 'playlist':
-        print("Playlist detected. Getting videos...")
-        page = Page_Xvideos(sUrl)
-        nPageStatus = page.content.status_code
-        if nPageStatus != 200:
-            if nPageStatus == 403:
-                raise ConnectionError(f"403 Forbidden! Please check if cookies are required! Private videos/playlists cannot be accessed without cookies!")
-
-        nNumPages = page._NumPagesGet()
-        sUrlBaseFormat = urlStandardize(sUrl)
-        print(f"Found {nNumPages} pages in the playlist...")
-
-        for nPage in range(0, nNumPages):
-            page = Page_Xvideos(sUrlBaseFormat.format(nPage))
-            page._extract_video_urls()
-            if page.videos:
-                lUrlVideos += page.videos
-                print(f"Found {len(page.videos)} on page {nPage + 1}")
-            else:
-                break
-
-        # Remove non-video URLs that may have been picked up
-        lTemp = []
-        for sUrl in lUrlVideos:
-            if 'com/videos/recommended' in sUrl:
-                continue
-            if 'com/video' in sUrl:
-                lTemp += [sUrl]
-        lUrlVideos = lTemp
-
-        nNumVideos = len(lUrlVideos)
-        print(f"Found {nNumVideos} video URLs in the playlist")
-        if bDebug:
-            for sUrl in lUrlVideos:
-                print(sUrl)
-
-    elif sUrlType == 'video':
-        lUrlVideos = [sUrl]
+    page = Page_Xvideos(sUrl)
+    nPageStatus = page.content.status_code
+    if nPageStatus != 200:
+        if nPageStatus == 403:
+            raise ConnectionError(f"403 Forbidden! Please check if cookies are required! Private videos/playlists cannot be accessed without cookies!")
 
     dYdlOptions = dict(dl_common.dYdlOptions)
     dYdlOptions['download_archive'] = rf".\\sites\\{sExtractor}\\{dYdlOptions['download_archive'].format(sExtractor)}"
 
-    for nIdx, sVideoUrl in enumerate(lUrlVideos):
-        if sUrlType == 'playlist':
-            print(f"Processing playlist video {nIdx + 1} of {nNumVideos} :: {sVideoUrl}")
+    print()
+    for nIdx, sVideoUrl in enumerate(page.videos):
+        if page.sUrlType == 'playlist':
+            print(f"Processing playlist video {nIdx + 1} of {len(page.videos)} :: {sVideoUrl}")
             print()
 
         dYdlOptions['outtmpl'] = rf'.\\sites\\{sExtractor}\\%(title)s.%(ext)s'
@@ -97,23 +57,89 @@ def run(sUrl, sCookieSource=None, nVideoLimit=None, bDebug=False):
 
 class Page_Xvideos(dl_common.Page):
 
+    def __init__(self, url):
+        super().__init__(url)
+        if f'{sExtractor}.com/video' in self.url:
+            sUrlType = 'video'
+        elif f'{sExtractor}.com/favorite' in self.url:
+            sUrlType = 'playlist'
+        else:
+            raise ValueError(f"Unable to determine {sExtractor} URL type for {self.url}! Please submit a bug report!")
+        self.sUrlType = sUrlType
+
+        self._sUrlBaseFormat = urlStandardize(self.url)
+
+        if self.sUrlType == 'video':
+            self.videos.append(self.url)
+        elif self.sUrlType == 'playlist':
+            print("Playlist detected. Getting videos...")
+            self._extract_video_urls()
+
+
     def _extract_video_urls(self, sFilter=None):
-        for a in self.soup.find_all('a', href=True):
+        """
+        Extract video URLs from all playlist pages.
+        """
+        nNumPages = self._NumPagesGet()
+        print(f"Found {nNumPages} pages in the playlist...")
+
+        lUrlVideos = []
+        for nPage in range(0, nNumPages):
+            lPageVideos = self._extract_page_urls(nPage)
+            if lPageVideos:
+                lUrlVideos += lPageVideos
+                print(f"Found {len(lPageVideos)} on page {nPage + 1}")
+            else:
+                print(f"Failed to load page {nPage + 1}!")
+                break
+
+        # Remove non-video URLs that may have been picked up
+        lTemp = []
+        for sUrl in lUrlVideos:
+            if 'com/videos/recommended' in sUrl:
+                continue
+            if 'com/video' in sUrl:
+                lTemp += [sUrl]
+            else:
+                raise ValueError(f"Not sure about this one: {sUrl}")
+        lUrlVideos = lTemp
+
+        nNumVideos = len(lUrlVideos)
+        print(f"\r\nFound {nNumVideos} video URLs in the playlist")
+        self.videos += lUrlVideos
+
+
+    def _extract_page_urls(self, nPage, sFilter=None):
+        """
+        Extract video URLs from a single playlist page.
+        """
+        sUrlPage = self._sUrlBaseFormat.format(nPage)
+        content = dl_common.session.get(sUrlPage, cookies=dl_common.dCookiesParsed)
+        soup = dl_common.BeautifulSoup(content.text, 'html.parser')
+
+        lVideos = []
+        for a in soup.find_all('a', href=True):
             href = a['href']
             if '/video' != href[:6]:
                 continue
             if '/videos-i-like' == href:
                 continue
+
             if sFilter is not None and sFilter not in href:
                 continue
 
-            if href not in self.videos:
-                sVideoUrlFull = 'https://www.xvideos.com' + a['href']
-                sVideoUrlSplit = sVideoUrlFull.split('?pl=')[0]
-                self.videos.append(sVideoUrlSplit)
+            sVideoUrlFull = 'https://www.xvideos.com' + a['href']
+            sVideoUrlSplit = sVideoUrlFull.split('?pl=')[0]
+
+            if sVideoUrlSplit not in lVideos:
+                lVideos.append(sVideoUrlSplit)
+        return lVideos
 
 
     def _NumPagesGet(self):
+        """
+        Return the number of pages in the playlist.
+        """
         pagination_block = self.soup.find(attrs={"class": 'pagination'})
 
         # Check for playlist with many pages first (i.e. pagination of pages)
